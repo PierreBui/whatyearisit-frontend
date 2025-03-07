7a09995d4a860b23428ea864b63289d2c34b6062
PierreBui <buis.p@icloud.com>
2025-03-07 14:10:18 +0100

    vercel

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-fetch('https://whatyearisit-backend-nine-pi.vercel.app')
+fetch('https://whatyearisit-frontend-mu-ochre.vercel.app/year')
 .then(response => response.json())
 .then(data => document.getElementById('year').textContent = data.year);
